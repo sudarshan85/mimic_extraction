67977415059e6af6325f58342e8181f2d3a20a2f
Sudarshan Srinivasan <sudarshan85@gmail.com>
2019-04-10 15:08:27 -0400

    refactor

--- extract.sql
+++ extract.sql
@@ -12,16 +12,6 @@ select pat.subject_id, pat.gender, pat.dob, pat.dod
 -- icu level factors
 , icu.icustay_id, icu.intime, icu.outtime, icu.los
 
--- noteevents
-, ne.charttime, ne.storetime, ne.category, ne.description, ne.text
-
--- in hours 
-, round((cast(extract(epoch from ne.storetime - adm.admittime)/(60*60) as numeric)), 2) as write
-
-, case
-  when cast(extract(epoch from ne.storetime - adm.admittime)/(60*60) as numeric) < cast(extract(epoch from icu.intime - adm.admittime)/(60*60) as numeric) then true
-  else false end as note_flag
-
 -- in years
 , round((cast(extract(epoch from adm.admittime - pat.dob)/(60*60*24*365.242) as numeric)), 2) as
 admission_age
@@ -46,16 +36,34 @@ los_hospital
   when dense_rank() over (partition by adm.hadm_id order by icu.intime) = 1 then true
   else false end as first_icu_stay
 
-from icustays icu
+from patients pat
 inner join admissions adm
-    on icu.hadm_id = adm.hadm_id
-inner join patients pat
-    on adm.subject_id = pat.subject_id
-inner join proxy_ne ne
-    on ne.hadm_id = adm.hadm_id
+  on adm.subject_id = pat.subject_id
+inner join icustays icu
+  on icu.hadm_id = adm.hadm_id
 where adm.has_chartevents_data = 1
-and ne.iserror is null
 order by pat.subject_id, adm.admittime, icu.intime;
+
+-- from icustays icu
+-- inner join admissions adm
+    -- on icu.hadm_id = adm.hadm_id
+-- inner join patients pat
+    -- on adm.subject_id = pat.subject_id
+-- inner join proxy_ne ne
+    -- on ne.hadm_id = adm.hadm_id
+-- where adm.has_chartevents_data = 1
+-- and ne.iserror is null
+-- order by pat.subject_id, adm.admittime, icu.intime;
+
+-- noteevents
+-- , ne.charttime, ne.storetime, ne.category, ne.description, ne.text
+
+-- -- in hours 
+-- , round((cast(extract(epoch from ne.storetime - adm.admittime)/(60*60) as numeric)), 2) as write
+
+-- , case
+  -- when cast(extract(epoch from ne.storetime - adm.admittime)/(60*60) as numeric) < cast(extract(epoch from icu.intime - adm.admittime)/(60*60) as numeric) then true
+  -- else false end as note_flag
 
 -- sequence of hospital admissions 
 -- , dense_rank() over (partition by pat.subject_id order by adm.admittime) as hospstay_seq
