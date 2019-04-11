9fbc41f57a2847c46ef0917b55ff49c89f8c630a
Sudarshan Srinivasan <sudarshan85@gmail.com>
2019-04-11 19:16:15 -0400

    Revamp

--- single.sql
+++ single.sql
@@ -2,11 +2,13 @@ drop materialized view if exists patadm cascade;
 create materialized view patadm as
 
 select pat.subject_id
-, pat.dob
+, pat.dob, pat.dod
 , adm.hadm_id, adm.admittime, adm.dischtime
 -- age at admission in years
 , round((cast(extract(epoch from adm.admittime - pat.dob)/(60*60*24*365.242) as numeric)), 2) as
 admission_age
+-- death period hospital in hours
+, round((cast(extract(epoch from pat.dod - adm.admittime)/(60*60) as numeric)), 2) as death_period
 -- los hospital in hours
 , round((cast(extract(epoch from adm.dischtime - adm.admittime)/(60*60) as numeric)), 2) as
 los_hospital
@@ -44,59 +46,72 @@ from admissions adm
 inner join icustays icu
   on icu.hadm_id = adm.hadm_id;
 
-drop materialized view if exists admne cascade;
-create materialized view admne as
-
-select adm.hadm_id
-, adm.admittime
-, ne.charttime as ne_charttime
-
--- time period between hospital admission and the note charttime in hours
-, round((cast(extract(epoch from ne.charttime - adm.admittime)/(60*60) as numeric)), 2) as
-ne_adm_period 
-
-from admissions adm
-inner join noteevents ne
-  on ne.hadm_id = adm.hadm_id;
-
-drop materialized view if exists admech cascade;
-create materialized view admech as
-
-select adm.hadm_id
-, adm.admittime
-, ech.charttime as ech_charttime
-
--- time period between echo hospital admission and echo charttime
-, round((cast(extract(epoch from ech.charttime - adm.admittime)/(60*60) as numeric)), 2) as
-ech_adm_period
-
-from admissions adm
-inner join echodata ech
-  on ech.hadm_id = adm.hadm_id;
-
 drop materialized view if exists co cascade;
 create materialized view co as
 
 select pa.subject_id
-, pa.dob, pa.hadm_id, pa.admittime, pa.admission_age, pa.dischtime, pa.los_hospital
+, pa.dob, pa.dod, pa.hadm_id, pa.admittime, pa.dischtime, pa.admission_age, pa.death_period,
+pa.los_hospital
 , ai.icustay_id, ai.intime, ai.wait_period, ai.include_adm, ai.include_icu
-, an.ne_charttime, an.ne_adm_period
-, ae.ech_charttime, ae.ech_adm_period
-
--- time period between note charttime and first icu visit in hours
-, round((cast(extract(epoch from ai.intime - an.ne_charttime)/(60*60) as numeric)), 2) as
-icu_ne_period
-
--- time period between echo hospital admission and echo charttime
-, round((cast(extract(epoch from ai.intime - ae.ech_charttime)/(60*60) as numeric)), 2) as
-icu_ech_period
 
 from patadm pa
 inner join admicu ai
   on ai.hadm_id = pa.hadm_id
-inner join admne an
-  on an.hadm_id = pa.hadm_id
-inner join admech ae
-  on ae.hadm_id = pa.hadm_id
 order by pa.subject_id, pa.admittime;
+
+-- drop materialized view if exists admne cascade;
+-- create materialized view admne as
+
+-- select adm.hadm_id
+-- , adm.admittime
+-- , ne.charttime as ne_charttime
+
+-- -- time period between hospital admission and the note charttime in hours
+-- , round((cast(extract(epoch from ne.charttime - adm.admittime)/(60*60) as numeric)), 2) as
+-- ne_adm_period 
+
+-- from admissions adm
+-- inner join noteevents ne
+  -- on ne.hadm_id = adm.hadm_id;
+
+-- drop materialized view if exists admech cascade;
+-- create materialized view admech as
+
+-- select adm.hadm_id
+-- , adm.admittime
+-- , ech.charttime as ech_charttime
+
+-- -- time period between echo hospital admission and echo charttime
+-- , round((cast(extract(epoch from ech.charttime - adm.admittime)/(60*60) as numeric)), 2) as
+-- ech_adm_period
+
+-- from admissions adm
+-- inner join echodata ech
+  -- on ech.hadm_id = adm.hadm_id;
+
+-- drop materialized view if exists co cascade;
+-- create materialized view co as
+
+-- select pa.subject_id
+-- , pa.dob, pa.hadm_id, pa.admittime, pa.admission_age, pa.dischtime, pa.los_hospital
+-- , ai.icustay_id, ai.intime, ai.wait_period, ai.include_adm, ai.include_icu
+-- , an.ne_charttime, an.ne_adm_period
+-- , ae.ech_charttime, ae.ech_adm_period
+
+-- -- time period between note charttime and first icu visit in hours
+-- , round((cast(extract(epoch from ai.intime - an.ne_charttime)/(60*60) as numeric)), 2) as
+-- icu_ne_period
+
+-- -- time period between echo hospital admission and echo charttime
+-- , round((cast(extract(epoch from ai.intime - ae.ech_charttime)/(60*60) as numeric)), 2) as
+-- icu_ech_period
+
+-- from patadm pa
+-- inner join admicu ai
+  -- on ai.hadm_id = pa.hadm_id
+-- inner join admne an
+  -- on an.hadm_id = pa.hadm_id
+-- inner join admech ae
+  -- on ae.hadm_id = pa.hadm_id
+-- order by pa.subject_id, pa.admittime;
 
