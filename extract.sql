ad2ef7841705e94d5a7fef10da91555cae96bf1a
Sudarshan Srinivasan <sudarshan85@gmail.com>
2019-04-10 21:32:12 -0400

    Rewrite

--- extract.sql
+++ extract.sql
@@ -1,122 +1,36 @@
 drop materialized view if exists co cascade;
 create materialized view co as
 
+select
 -- patient level factors
-select pat.subject_id, pat.gender, pat.dob, pat.dod
-
--- hospital level factors
-, adm.hadm_id, adm.admittime, adm.dischtime, adm.deathtime, adm.admission_type
-, adm.insurance, adm.language, adm.religion, adm.marital_status, adm.ethnicity
-, adm.diagnosis
-
+pat.subject_id, pat.dob,
+-- hospital admission level factors
+adm.hadm_id, adm.admittime,
 -- icu level factors
-, icu.icustay_id, icu.intime, icu.outtime, icu.los
+icu.icustay_id, icu.intime,
 
--- noteevents
-, ne.chartdate, ne.charttime, ne.storetime, ne.category, ne.description, ne.text
--- in hours 
-, round((cast(extract(epoch from ne.storetime - ne.charttime)/(60*60) as numeric)), 2) as
-store_period
+-- age at admission in years
+round((cast(extract(epoch from adm.admittime - pat.dob)/(60*60*24*365.242) as numeric)), 2) as
+admission_age,
 
--- in hours 
-, round((cast(extract(epoch from ne.charttime - adm.admittime)/(60*60) as numeric)), 2) as
-note_period
-, case
-  when cast(extract(epoch from ne.charttime - adm.admittime)/(60*60) as numeric) <
-    cast(extract(epoch from icu.intime - adm.admittime)/(60*60) as numeric) then true
-  else false end as note_flag 
+lag(adm.admittime, 1) over (partition by pat.subject_id order by adm.admittime) as prev,
 
--- in years
-, round((cast(extract(epoch from adm.admittime - pat.dob)/(60*60*24*365.242) as numeric)), 2) as
-admission_age
--- in days
-, round((cast(extract(epoch from adm.dischtime - adm.admittime)/(60*60*24) as numeric)), 2) as
-los_hospital
-
--- wait time between hospital admission and icu intime in hours
-, round((cast(extract(epoch from icu.intime - adm.admittime)/(60*60) as numeric)), 2) as wait_period
+dense_rank() over (partition by pat.subject_id order by adm.admittime) as adm_seq,
 
 -- mark the first hospital stay
-, case
-  when dense_rank() over (partition by pat.subject_id order by adm.admittime) = 1 then true
--- include in first_hosp_stay if its been atleast a month since prvious admission. Using lag() as
--- shown here: http://bit.ly/2KpJaeg
-  when round((cast(extract(epoch from adm.admittime - lag(adm.admittime, 1) over (partition by
-    pat.subject_id order by adm.admittime) )/(60*60*30) as numeric)), 2) > 30.0 then true
-  else false end as adm_flag
+case when dense_rank() over (partition by pat.subject_id order by adm.admittime) = 1 then true else
+  false end as include_adm1,
 
--- mark the first icu stay for current hospital admission
-, case
-  when dense_rank() over (partition by adm.hadm_id order by icu.intime) = 1 then true
-  else false end as icu_flag
+round((cast(extract(epoch from adm.admittime - lag(adm.admittime, 1) over (partition by pat.subject_id order by adm.admittime))/(60*60*24) as numeric)), 2) as adm_delta,
+
+case when round((cast(extract(epoch from adm.admittime - lag(adm.admittime, 1) over (partition by
+  pat.subject_id order by adm.admittime))/(60*60*24) as numeric)), 2) > 30.0 then true
+  when dense_rank() over (partition by pat.subject_id order by adm.admittime) = 1 then true
+  else false end as include_adm2
 
 from patients pat
 inner join admissions adm
   on adm.subject_id = pat.subject_id
 inner join icustays icu
   on icu.hadm_id = adm.hadm_id
-inner join noteevents ne
-  on ne.hadm_id = adm.hadm_id
-where adm.has_chartevents_data = 1
-and ne.iserror is null
--- and ne.charttime between adm.admittime and icu.intime
 order by pat.subject_id, adm.admittime, icu.intime;
-
--- discard records where admittime is > icu intime
--- and round((cast(extract(epoch from icu.intime - adm.admittime)/(60*60) as numeric)), 4)  > 0.0
-
--- -- in hours 
--- , round((cast(extract(epoch from ne.storetime - adm.admittime)/(60*60) as numeric)), 2) as write
-
--- , case
-  -- when cast(extract(epoch from ne.storetime - adm.admittime)/(60*60) as numeric) < cast(extract(epoch from icu.intime - adm.admittime)/(60*60) as numeric) then true
-  -- else false end as note_flag
-
--- sequence of hospital admissions 
--- , dense_rank() over (partition by pat.subject_id order by adm.admittime) as hospstay_seq
--- sequence of icu admissions for current hospital admission
--- , dense_rank() over (partition by adm.hadm_id order by icu.intime) as icustay_seq
-
--- , lag(adm.admittime, 1) over (partition by pat.subject_id order by adm.admittime) as prev
--- , round((cast(extract(epoch from adm.admittime - lag(adm.admittime, 1) over (partition by pat.subject_id order by adm.admittime) )/(60*60*30) as numeric)), 2) as diff -- in months
-
--- and round((cast(extract(epoch from icu.intime - adm.admittime)/(60*60) as numeric)), 4)  > 0.0
--- icu level factors
--- select ie.subject_id, ie.hadm_id, ie.icustay_id, ie.intime, ie.outtime, ie.los
-
--- -- patient level factors
--- , pat.gender, pat.dob, pat.dod
-
--- -- hospital level factors
--- , adm.admittime, adm.dischtime
--- , round((cast(extract(epoch from adm.dischtime - adm.admittime)/(60*60*24) as numeric)), 4) as los_hospital -- in days
--- , round((cast(extract(epoch from adm.admittime - pat.dob)/(60*60*24*365.242) as numeric)), 4) as admission_age -- in years
--- , adm.deathtime, adm.admission_type, adm.admission_location, adm.discharge_location, adm.insurance
--- , adm.language, adm.religion, adm.marital_status, adm.ethnicity
-
--- -- wait time between hospital admission and icu intime
--- , round((cast(extract(epoch from ie.intime - adm.admittime)/(60*60) as numeric)), 4) as waiting -- in hours 
-
--- -- sequence of hospital admissions 
--- , dense_rank() over (partition by adm.subject_id order by admittime) as hospstay_seq
-
--- -- mark the first hospital stay
--- , case
-  -- when dense_rank() over (partition by adm.subject_id order by adm.admittime) = 1 then true
-  -- else false end as first_hosp_stay
-
--- -- sequence of icu admissions for current hospital admission
--- , dense_rank() over (partition by ie.hadm_id order by ie.intime) as icustay_seq
-
--- -- mark the first icu stay for current hospital admission
--- , case
-  -- when dense_rank() over (partition by ie.hadm_id order by ie.intime) = 1 then true
-  -- else false end as first_icu_stay
-
--- from icustays ie
--- inner join admissions adm
-    -- on ie.hadm_id = adm.hadm_id
--- inner join patients pat
-    -- on ie.subject_id = pat.subject_id
--- where adm.has_chartevents_data = 1
--- order by ie.subject_id, adm.admittime, ie.intime;
