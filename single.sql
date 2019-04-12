7e4035a63d7d3a5d12754a9d6a7f1bdd7f980472
Sudarshan Srinivasan <sudarshan85@gmail.com>
2019-04-12 12:11:51 -0400

    Removed periods. Next grabbing all columns

--- single.sql
+++ single.sql
@@ -1,21 +1,10 @@
-drop materialized view if exists patadm cascade;
-create materialized view patadm as
+-- ------------------------------------------------------------------
+-- Title: 
+-- Description: 
+-- MIMIC version: MIMIC-III v1.4
+-- ------------------------------------------------------------------
 
-select pat.subject_id
-, pat.dob, pat.dod
-, adm.hadm_id, adm.admittime, adm.dischtime
--- age at admission in years
-, round((cast(extract(epoch from adm.admittime - pat.dob)/(60*60*24*365.242) as numeric)), 2) as
-admission_age
--- los hospital in hours
-, round((cast(extract(epoch from adm.dischtime - adm.admittime)/(60*60) as numeric)), 2) as
-los_hospital
-
-from patients pat
-inner join admissions adm
-  on adm.subject_id = pat.subject_id
-where adm.has_chartevents_data = 1;
-
+-- Grab data from icustays table joining on hadm_id
 drop materialized view if exists admicu cascade;
 create materialized view admicu as
 
@@ -44,6 +33,7 @@ from admissions adm
 inner join icustays icu
   on icu.hadm_id = adm.hadm_id;
 
+-- Grab data from noteevents table joining on hadm_id
 drop materialized view if exists admne cascade;
 create materialized view admne as
 
@@ -51,29 +41,12 @@ select adm.hadm_id
 , adm.admittime
 , ne.charttime as ne_charttime
 
--- time period between hospital admission and the note charttime in hours
-, round((cast(extract(epoch from ne.charttime - adm.admittime)/(60*60) as numeric)), 2) as
-ne_adm_period 
-
 from admissions adm
 inner join noteevents ne
   on ne.hadm_id = adm.hadm_id;
 
-drop materialized view if exists admne cascade;
-create materialized view admne as
-
-select adm.hadm_id
-, adm.admittime
-, ne.charttime as ne_charttime
-
--- time period between hospital admission and the note charttime in hours
-, round((cast(extract(epoch from ne.charttime - adm.admittime)/(60*60) as numeric)), 2) as
-ne_adm_period 
-
-from admissions adm
-inner join noteevents ne
-  on ne.hadm_id = adm.hadm_id;
-
+-- Grab data between echodata materialized view created by http://bit.ly/2Zh3phU
+-- joinong on hadm_id
 drop materialized view if exists admech cascade;
 create materialized view admech as
 
@@ -81,30 +54,38 @@ select adm.hadm_id
 , adm.admittime
 , ech.charttime as ech_charttime
 
--- time period between echo hospital admission and echo charttime
-, round((cast(extract(epoch from ech.charttime - adm.admittime)/(60*60) as numeric)), 2) as
-ech_adm_period
-
 from admissions adm
 inner join echodata ech
   on ech.hadm_id = adm.hadm_id;
 
+-- Grab data from the patients table joining on subject_id
+drop materialized view if exists patadm cascade;
+create materialized view patadm as
+
+select pat.subject_id
+, pat.dob, pat.dod
+, adm.hadm_id, adm.admittime, adm.dischtime
+-- age at admission in years
+, round((cast(extract(epoch from adm.admittime - pat.dob)/(60*60*24*365.242) as numeric)), 2) as
+admission_age
+-- los hospital in hours
+, round((cast(extract(epoch from adm.dischtime - adm.admittime)/(60*60) as numeric)), 2) as
+los_hospital
+
+from patients pat
+inner join admissions adm
+  on adm.subject_id = pat.subject_id
+where adm.has_chartevents_data = 1;
+
+-- Finally put all the data together in ta final materiazlied view joining on hadm_id
 drop materialized view if exists co cascade;
 create materialized view co as
 
 select pa.subject_id
 , pa.dob, pa.hadm_id, pa.admittime, pa.dischtime, pa.admission_age, pa.los_hospital
 , ai.icustay_id, ai.intime, ai.wait_period, ai.include_adm, ai.include_icu
-, an.ne_charttime, an.ne_adm_period
-, ae.ech_charttime, ae.ech_adm_period
-
--- time period between note charttime and first icu visit in hours
-, round((cast(extract(epoch from ai.intime - an.ne_charttime)/(60*60) as numeric)), 2) as
-icu_ne_period
-
--- time period between echo hospital admission and echo charttime
-, round((cast(extract(epoch from ai.intime - ae.ech_charttime)/(60*60) as numeric)), 2) as
-icu_ech_period
+, an.ne_charttime 
+, ae.ech_charttime
 
 from patadm pa
 inner join admicu ai
