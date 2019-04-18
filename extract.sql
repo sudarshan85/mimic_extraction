d115e1fb3005f6e65481e476c12b659eae9dda85
Sudarshan Srinivasan <sudarshan85@gmail.com>
2019-04-18 12:07:08 -0400

    Duplicates

--- extract.sql
+++ extract.sql
@@ -63,8 +63,100 @@ select ai.hadm_id, ai.subject_id, ai.icustay_id, age.admission_age, ai.admittime
 , ai.los_hospital, ae.charttime, ai.intime, ai.wait_period
 , ae.category, ae.description, ae.text
 
-  -- time period between charttime and ICU intime
-  , round((cast(extract(epoch from ai.intime- ae.charttime)/(60*60) as numeric)), 2) as chart_period
+-- time period between charttime and ICU intime
+, round((cast(extract(epoch from ai.intime- ae.charttime)/(60*60) as numeric)), 2) as chart_period
+
+, case
+  when ae.charttime between ai.intime - interval '1 day' and ai.intime then 0
+  when ae.charttime between ai.intime - interval '2 days' and ai.intime - interval '1 day' then 1
+  when ae.charttime between ai.intime - interval '3 days' and ai.intime - interval '2 days' then 2
+  when ae.charttime between ai.intime - interval '4 days' and ai.intime - interval '3 days' then 3
+  when ae.charttime between ai.intime - interval '5 days' and ai.intime - interval '4 days' then 4
+  when ae.charttime between ai.intime - interval '6 days' and ai.intime - interval '5 days' then 5
+  when ae.charttime between ai.intime - interval '7 days' and ai.intime - interval '6 days' then 6
+  when ae.charttime between ai.intime - interval '8 days' and ai.intime - interval '7 days' then 7
+  when ae.charttime between ai.intime - interval '9 days' and ai.intime - interval '8 days' then 8
+  when ae.charttime between ai.intime - interval '10 days' and ai.intime - interval '9 days' then 9
+  when ae.charttime between ai.intime - interval '11 days' and ai.intime - interval '10 days' then
+    10 
+  when ae.charttime between ai.intime - interval '12 days' and ai.intime - interval '11 days' then
+    11 
+  when ae.charttime between ai.intime - interval '13 days' and ai.intime - interval '12 days' then
+    12 
+  when ae.charttime between ai.intime - interval '14 days' and ai.intime - interval '13 days' then
+    13 
+  when ae.charttime between ai.intime - interval '15 days' and ai.intime - interval '14 days' then
+    14 
+  else 15 end as chartinterval
+
+-- , case
+  -- when ae.charttime between ai.intime - interval '24 hours' and ai.intime then -1
+  -- when ae.charttime between ai.intime - interval '96 hours' and ai.intime - interval '24 hours' then
+    -- 1
+  -- when ae.charttime between ai.intime - interval '192 hours' and ai.intime - interval '96 hours' then
+    -- -1
+  -- else 0 end as class_label 
+
+from admicu ai
+inner join admne ae
+  on ae.hadm_id = ai.hadm_id
+inner join age
+  on age.subject_id = ai.subject_id
+where
+-- subjects should have recorded chartevents data
+ai.has_chartevents_data = 1 and
+-- discard subjects who have discharge time earlier than admittime
+ai.dischtime > ai.admittime and
+-- discard subjects who have ICU intime earlier than admittime
+ai.intime > ai.admittime and
+-- only include subjects with one admission or previous admission more than 30 days ago
+ai.include_adm = true and
+-- only include subjects' first ICU visit for that admission
+ai.include_icu = true and
+-- only include adult subjects
+age.admission_age >= 15.0 and
+-- only include notes which are chartted between admittime and ICU intime
+ae.charttime between admittime and intime and
+-- discard documented erroneous notes
+ae.iserror is null 
+order by ai.hadm_id, ai.intime;
+
+-- , case
+  -- when ae.charttime between ai.intime - interval '24 hours' and ai.intime then 0
+  -- when ae.charttime between ai.intime - interval '48 hours' and ai.intime - interval '24 hours' then
+    -- 1
+  -- when ae.charttime between ai.intime - interval '72 hours' and ai.intime - interval '48 hours' then
+    -- 2
+  -- when ae.charttime between ai.intime - interval '96 hours' and ai.intime - interval '72 hours' then
+    -- 3
+  -- when ae.charttime between ai.intime - interval '120 hours' and ai.intime - interval '96 hours'
+    -- then 4
+  -- when ae.charttime between ai.intime - interval '144 hours' and ai.intime - interval '120 hours'
+    -- then 5
+  -- when ae.charttime between ai.intime - interval '168 hours' and ai.intime - interval '144 hours'
+    -- then 6
+  -- when ae.charttime between ai.intime - interval '192 hours' and ai.intime - interval '168 hours'
+    -- then 7
+  -- when ae.charttime between ai.intime - interval '216 hours' and ai.intime - interval '192 hours'
+    -- then 8
+  -- when ae.charttime between ai.intime - interval '240 hours' and ai.intime - interval '216 hours'
+    -- then 9
+  -- when ae.charttime between ai.intime - interval '264 hours' and ai.intime - interval '240 hours'
+    -- then 10
+  -- when ae.charttime between ai.intime - interval '288 hours' and ai.intime - interval '264 hours'
+    -- then 11
+  -- when ae.charttime between ai.intime - interval '312 hours' and ai.intime - interval '288 hours'
+    -- then 12
+  -- else 13 end as chartinterval
+
+-- , case
+  -- when ae.charttime between ai.intime - interval '24 hours' and ai.intime then -1
+  -- when ae.charttime between ai.intime - interval '96 hours' and ai.intime - interval '24 hours' then
+    -- 1
+  -- when ae.charttime between ai.intime - interval '192 hours' and ai.intime - interval '96 hours' then
+    -- -1
+  -- else 0 end as class_label 
+
 
 -- , case
   -- when ae.charttime between ai.intime - interval '180 hours' and ai.intime then 0
@@ -85,34 +177,6 @@ select ai.hadm_id, ai.subject_id, ai.icustay_id, age.admission_age, ai.admittime
   -- when ae.charttime between ai.intime - interval '276 hours' and ai.intime - interval '264 hours'
     -- then 8
   -- else 9 end as chartinterval
-
-, case
-  when ae.charttime between ai.intime - interval '24 hours' and ai.intime then 0
-  when ae.charttime between ai.intime - interval '48 hours' and ai.intime - interval '24 hours' then
-    1
-  when ae.charttime between ai.intime - interval '72 hours' and ai.intime - interval '48 hours' then
-    2
-  when ae.charttime between ai.intime - interval '96 hours' and ai.intime - interval '72 hours' then
-    3
-  when ae.charttime between ai.intime - interval '120 hours' and ai.intime - interval '96 hours'
-    then 4
-  when ae.charttime between ai.intime - interval '144 hours' and ai.intime - interval '120 hours'
-    then 5
-  when ae.charttime between ai.intime - interval '168 hours' and ai.intime - interval '144 hours'
-    then 6
-  when ae.charttime between ai.intime - interval '192 hours' and ai.intime - interval '168 hours'
-    then 7
-  when ae.charttime between ai.intime - interval '216 hours' and ai.intime - interval '192 hours'
-    then 8
-  when ae.charttime between ai.intime - interval '240 hours' and ai.intime - interval '216 hours'
-    then 9
-  when ae.charttime between ai.intime - interval '264 hours' and ai.intime - interval '240 hours'
-    then 10
-  when ae.charttime between ai.intime - interval '288 hours' and ai.intime - interval '264 hours'
-    then 11
-  when ae.charttime between ai.intime - interval '312 hours' and ai.intime - interval '288 hours'
-    then 12
-  else 13 end as chartinterval
 
 -- , case
   -- when ae.charttime between ai.intime - interval '12 hours' and ai.intime then 0
@@ -145,27 +209,3 @@ select ai.hadm_id, ai.subject_id, ai.icustay_id, age.admission_age, ai.admittime
   -- when ae.charttime between ai.intime - interval '180 hours' and ai.intime - interval '168 hours'
     -- then 14
   -- else 15 end as chartinterval
-
-from admicu ai
-inner join admne ae
-  on ae.hadm_id = ai.hadm_id
-inner join age
-  on age.subject_id = ai.subject_id
-where
--- subjects should have recorded chartevents data
-ai.has_chartevents_data = 1 and
--- discard subjects who have discharge time earlier than admittime
-ai.dischtime > ai.admittime and
--- discard subjects who have ICU intime earlier than admittime
-ai.intime > ai.admittime and
--- only include subjects with one admission or previous admission more than 30 days ago
-ai.include_adm = true and
--- only include subjects' first ICU visit for that admission
-ai.include_icu = true and
--- only include adult subjects
-age.admission_age >= 15.0 and
--- only include notes which are chartted between admittime and ICU intime
-ae.charttime between admittime and intime and
--- discard documented erroneous notes
-ae.iserror is null 
-order by ai.hadm_id, ai.intime;
